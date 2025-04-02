481ce7e735b2187a29357e28cb26621ee68964e3
micah-home-laptop <mjooevermann@gmail.com>
2025-04-02 13:28:55 -0500

    added the controller functions

---
+++ my_controller.py
@@ -0,0 +1,47 @@
+from pydrake.all import LeafSystem
+import copy
+
+class FeedbackController(LeafSystem):
+    '''
+        DESCRIPTION
+    '''
+    def __init__(self, plant, model_idx):
+       
+        # drake specific setups
+        LeafSystem.__init__(self)
+        self.nstates = plant.num_multibody_states(model_idx)
+        self.nq = plant.num_positions(model_idx)
+        self.nv = plant.num_velocities(model_idx)
+
+        # declare avector input port the size of the robots state
+        self.DeclareVectorInputPort("robot_state", self.nstates)
+        
+        #declare an output port for the controller
+        self.DeclareVectorOutputPort("tau_m", plant.num_actuated_dofs(model_idx), self.CalcControlEffort)
+        self.plant_ = copy.copy(plant)
+        self.model_idx_ = model_idx
+        
+        
+    def CalcControlEffort(self, context, output):
+        # read in the states
+        sampled_states = self.GetInputPort("robot_state").Eval(context)
+        
+        # update what we think the plant is at
+        plant_context = self.plant_.CreateDefaultContext()
+        self.plant_.SetPositionsAndVelocities(plant_context, sampled_states)
+        
+        # build system matricies
+        # recall these are defined like
+        # M*dv + C*v = Ut*tau_m + tau_g
+        # see below for more info:
+        # https://drake.mit.edu/doxygen_cxx/classdrake_1_1multibody_1_1_multibody_plant.html
+        Cv = self.plant_.CalcBiasTerm(plant_context) # calcs C(q, v) v
+        Vq =  self.plant_.CalcGravityGeneralizedForces(plant_context)  # calcs tau_g(q)
+        Mq = self.plant_.CalcMassMatrix(plant_context) # calcs M(q)
+       
+        '''Do your control work here'''
+        tau_control = -Vq # git the manipulator the torques to hold steady over gravity
+        
+
+        # send the computed torues to the robot
+        output.SetFromVector(tau_control)
---
+++ run_simulator.py
@@ -0,0 +1,56 @@
+from pydrake.all import ( 
+    ModelVisualizer,
+    AddMultibodyPlant,
+    MultibodyPlantConfig,
+    DiagramBuilder,
+    Parser,
+    Meshcat,
+    AddDefaultVisualization,
+    Simulator
+)
+
+from my_controller import FeedbackController
+
+ur_path = "./ur_description/urdf/ur3e_cylinders_collision.urdf"
+
+# set up a diagram and meshcat viewer
+builder = DiagramBuilder()
+meshcat = Meshcat()
+
+# initialize the plant with the simulation specific parameters
+plant, scene_graph = AddMultibodyPlant(
+    MultibodyPlantConfig(
+        time_step=2e-3 # indicates that the solver should use a 500Hz timestep
+        ),builder=builder)
+parser = Parser(plant)
+model_idx = parser.AddModels(ur_path)[0]
+
+# weld the robot to the workspace or else it would just fall
+plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("ur_base_link"))
+# finish editing the plant
+plant.Finalize()
+
+# add our feedback controller block into the system diagram
+controller = builder.AddSystem(FeedbackController(plant, model_idx))
+
+# connect the input and output ports
+builder.Connect(controller.GetOutputPort("tau_m"), plant.get_actuation_input_port())
+builder.Connect(plant.get_state_output_port(), controller.GetInputPort('robot_state'))
+
+# connect the meshcat visualizer
+AddDefaultVisualization(builder, meshcat)
+
+# Finalize and Build the Diagram
+diagram = builder.Build()
+
+diagram_context = diagram.CreateDefaultContext()
+robot_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
+
+
+# simulate the system
+simulator = Simulator(diagram)
+
+meshcat.StartRecording()# these will add playback and video record buttons to meshcat
+simulator.set_target_realtime_rate(1)
+simulator.AdvanceTo(5) # final time to simulate to
+meshcat.PublishRecording()
